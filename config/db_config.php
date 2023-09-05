3bf262db8c15797111a5a2cad183242b3c293a83
TaeWon Park <thisis_tw94@naver.com>
2023-09-05 21:50:34 +0900

    pdo 변경

--- config/db_config.php
+++ config/db_config.php
@@ -1,9 +1,8 @@
 <?php
-$host = 'localhost:3306';
-$db = 'laundry';
-$user = 'root';
-$pass = '1234';
-$dbChar = "utf8";
-$charset = 'utf8mb4';
+$host = "localhost:3306";
+$db = "setak";
+$user = "root";
+$pass = "1234";
+$charset = "utf8";
 $dsn="mysql:host=$host;dbname=$db;charset=$charset;";
 ?>
